4378d82ab9e2d9039e964199270d9962dd4d61f8
siumhossain <sium.hossain@yahoo.com>
2020-11-21 22:48:40 +0600

    1st

---
+++ hello.py
@@ -0,0 +1,13 @@
+import kivy
+kivy.require('1.9.0')
+
+
+from kivy.app import App
+from kivy.uix.button import Label
+
+class HelloApp(App):
+	def build(self):
+		return Label(text='hello world')
+if __name__=="__main__":
+	HelloApp().run()
+
